2bbd4ae0e04e67201313a7c54c6cdf46e80e647f
Sumankes <107619932+Sumankes@users.noreply.github.com>
2022-06-16 16:54:29 +0530

    Update Arrayfile.java

--- Arrayfile.java
+++ Arrayfile.java
@@ -4,6 +4,7 @@ public class Arr2{
 		System.out.println(mat.length);
 		for(int i=0;i<mat.length;i++) {
 			for(int j=0;j<mat[i].length;j++) {
+				System.out.println(mat[i].length);//It will find length of each arr
 				System.out.println(mat[i][j]);
 				
 			}

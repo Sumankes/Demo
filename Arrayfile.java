83ece8e13f5e4e3ff6c90486637262d6a958dbc2
Sumankes <107619932+Sumankes@users.noreply.github.com>
2022-06-16 16:41:53 +0530

    Array updated

--- Arrayfile.java
+++ Arrayfile.java
@@ -1,6 +1,6 @@
 public class Arr2{
 	public static void main(String[]args) {
-		int mat[][]= {{1},{2,3},{4,5,6},{7,8,9,0}};
+		int mat[][]= {{1},{2,3},{4,5,6},{7,8,9,0},{1,2,3,4,5}};
 		System.out.println(mat.length);
 		for(int i=0;i<mat.length;i++) {
 			for(int j=0;j<mat[i].length;j++) {
